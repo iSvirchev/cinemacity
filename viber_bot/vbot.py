c70928760487f4e1973d37f815399d37cb0e835f
iSvirchev <i.svirchev@yahoo.com>
2022-07-20 15:36:31 +0300

    responses.py:     - Exported keyboard and text responses for viber message to a responses.py vbot.py:     - improved readability by exporting viber message responses data to a separate file

--- viber_bot/vbot.py
+++ viber_bot/vbot.py
@@ -4,7 +4,9 @@ import logging
 import requests
 
 from user_func import DatabaseCommunication
+from responses import Responses
 import paths
+
 from flask import Flask, request, Response
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
@@ -85,6 +87,8 @@ for day in days_dictionary:
 
 days_dictionary = remove_empty_elements(days_dictionary)  # this removes movies and days with no showings (pre-sales)
 
+rsp = Responses(days_dictionary)
+
 
 def broadcast_new_movies(diff_set):
     broadcast_msg = "(video) ```New movies in cinema this week!``` (video)\n\n"
@@ -113,92 +117,6 @@ if len(new_movies_set) != 0:
 
 days = list(days_dictionary.keys())
 
-dates_resp = 'Which day are you interested in?\n'
-for day_resp in days_dictionary:
-    dates_resp = dates_resp + '\n' + day_resp
-
-
-def generate_movies_response(movies_resp_day):
-    movies_resp = '*Movies currently in cinema for date %s*\n' % movies_resp_day
-    for movie_resp in days_dictionary[movies_resp_day]:
-        movies_resp = movies_resp + '\n' + movie_resp
-    return movies_resp
-
-
-info_resp = 'Please type one of the following commands:\n\n' \
-            '*Today* - will display today\'s movies on screen\n' \
-            '*Tomorrow* - will display tomorrow\'s movies on screen\n' \
-            '*Dates* - will provide you with buttons of dates from which you can choose.'
-
-
-def generate_btn_keyboard(days_arr):
-    keyboard = {
-        "Type": "keyboard",
-        "Buttons": []
-    }
-
-    colour_codes = ['#75ace1', '#df7779', '#d1b185', '#8187d5', '#00703c', '#7ac2dc', '#bc8dc9', '#727C96', '#D9CBC1',
-                    '#75ace1', '#df7779', '#d1b185', '#8187d5', '#00703c', '#7ac2dc', '#bc8dc9', '#727C96', '#D9CBC1',
-                    '#75ace1', '#df7779', '#d1b185', '#8187d5', '#00703c', '#7ac2dc', '#bc8dc9', '#727C96', '#D9CBC1']
-
-    button_tpl = {
-        "Columns": 2,
-        "Rows": 2,
-        "BgColor": "#e6f5ff",  # <enter_colour_code_here>
-        "BgLoop": True,
-        "ActionType": "reply",
-        "ActionBody": "<add_action_body>",
-        "Text": "<add_btn_txt>"
-    }
-    for day in days_arr:
-        day_btn = button_tpl.copy()  # we use .copy() as a simple assignment operator '=' gives us object reference
-        day_btn['ActionBody'] = day
-        day_btn['Text'] = '<font size=\"24\">%s</font>' % day
-        day_btn['BgColor'] = colour_codes[days_arr.index(day)]
-        keyboard['Buttons'].append(day_btn)
-
-    return keyboard
-
-
-days_kb = generate_btn_keyboard(days)
-
-
-def generate_movie_keyboard(movie_kb_day):
-    keyboard = {
-        "Type": "keyboard",
-        "Buttons": []
-    }
-
-    m_btn_tpl = {
-        "Columns": 2,
-        "Rows": 2,
-        "BgColor": "#e6f5ff",
-        "BgLoop": True,
-        "BgMedia": "<add_poster_link>",
-        "ActionType": "",
-        "ActionBody": "<add_action_body>",
-        "Text": "<add_btn_txt>"
-    }
-
-    for m_name, m_value in days_dictionary[movie_kb_day].items():
-        m_poster = m_value['poster_link']
-
-        day_btn = m_btn_tpl.copy()  # we use .copy() as a simple assignment operator '=' gives us object reference
-        day_btn['ActionBody'] = m_name
-        # day_btn['Text'] = "<font size=\"12\">%s</font>" % m_name
-        day_btn['Text'] = m_name
-        day_btn['BgMedia'] = m_poster
-        keyboard['Buttons'].append(day_btn)
-
-    return keyboard
-
-
-def gen_movie_resp(movie_resp_day, movie_resp_movie):
-    m_resp = 'Screenings of movie *%s* on *%s*\n\n' % (movie_resp_movie, movie_resp_day)
-    screenings = days_dictionary[movie_resp_day][movie_resp_movie]['movie_screenings']
-    m_resp = m_resp + '\n'.join(screenings)
-    return m_resp
-
 
 @app.route('/', methods=['POST'])
 def incoming():
@@ -221,7 +139,7 @@ def incoming():
 
         if message.lower() == 'dates':
             viber.send_messages(sender_id, [
-                TextMessage(text=dates_resp, keyboard=days_kb)
+                TextMessage(text=rsp.dates(), keyboard=rsp.days_keyboard(days))
             ])
         elif message in days_dictionary or message.lower() == 'today' or message.lower() == 'tomorrow':
             # message here equals the date or today or tomorrow
@@ -235,8 +153,8 @@ def incoming():
             db.set_user_date(sender_id, sel_day)
             logger.info("SENDER_ID: '%s' has selected a new day: '%s'" % (sender_id, sel_day))
             try:
-                reply = generate_movies_response(sel_day)
-                kb = generate_movie_keyboard(sel_day)
+                reply = rsp.movies(sel_day)
+                kb = rsp.movie_keyboard(sel_day)
             except KeyError as ke:
                 reply = "No movie screenings for the selected day: *%s*" % sel_day
                 kb = None
@@ -248,14 +166,14 @@ def incoming():
         elif message in days_dictionary[sender_sel_date]:  # message here equals the name of the movie
             logger.info(
                 "SENDER_ID: '%s' selected movie '%s' for day '%s'" % (sender_id, message, sender_sel_date))
-            reply = gen_movie_resp(sender_sel_date, message)
+            reply = rsp.movie(sender_sel_date, message)
             poster = days_dictionary[sender_sel_date][message]['poster_link']
             viber.send_messages(sender_id, [
                 PictureMessage(text=reply, media=poster)
             ])
         else:
             viber.send_messages(sender_id, [
-                TextMessage(text=info_resp)
+                TextMessage(text=rsp.info)
             ])
     elif isinstance(viber_request, ViberSubscribedRequest):
         viber.send_messages(viber_request.user.id, [
@@ -263,7 +181,7 @@ def incoming():
                              'Type *INFO* for more information on the available commands.' % viber_request.user.name)
         ])
     elif isinstance(viber_request, ViberConversationStartedRequest):
-        reply = 'Welcome %s!\n\n%s' % (viber_request.user.name, info_resp)
+        reply = 'Welcome %s!\n\n%s' % (viber_request.user.name, rsp.info)
         viber.send_messages(viber_request.user.id, [
             TextMessage(text=reply)
         ])
