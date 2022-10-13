e6b40f44c837071d0137979f544b3dc40ec4e7bc
iSvirchev <i.svirchev@yahoo.com>
2022-10-13 15:14:38 +0300

    broadcaster.py:      - changed broadcast_new_movies()'s signature to accept cinema_name instea of cinema_id and removed unnecessary DB select of cinema_name      - improved failed_list handling from the list of failed users of a response (if broadcast to 1 user succeeds and the rest fail - status_message is still OK)      - improved logging

--- viber_bot/broadcaster.py
+++ viber_bot/broadcaster.py
@@ -21,11 +21,9 @@ with open(paths.CONFIG_PATH, 'r') as f:
 logger.info("bot_token extracted")
 
 
-def broadcast_new_movies(diff_set, broadcast_list, cin_id):
-    logger.info("Starting a broadcast...")
-    cinema_name = db.fetch_cinema_by_id(cin_id, CinemasTable.CINEMA_NAME)
-    broadcast_msg = "(video) ```New movies in ```*%s*``` this week!``` (video)\n\n" % cinema_name
-
+def broadcast_new_movies(diff_set, broadcast_list, cin_name):
+    broadcast_msg = "(video) ```New movies in ```*%s*``` this week!``` (video)\n\n" % cin_name
+    logger.info("Movies to broadcast: %s", str(diff_set))
     for new_movie in diff_set:
         broadcast_msg = broadcast_msg + "*%s*\n" % new_movie
 
@@ -38,27 +36,33 @@ def broadcast_new_movies(diff_set, broadcast_list, cin_id):
                          headers={"X-Viber-Auth-Token": bot_token})
 
     resp_json = resp.json()
-    status_msg = resp_json['status']
-
-    if status_msg == ErrorCodes.OK:
+    failed_list = resp_json['failed_list']
+    logger.info("Broadcast failed for %d users!", len(failed_list))
+    if len(failed_list) == 0:
         logger.info("Successfully broadcasted a message to the following users: %s", str(broadcast_list))
     else:
-        failed_list = resp_json['failed_list'][0]
-        failed_user_id = failed_list['receiver']
-        failed_status_code = failed_list['status']
-        failed_status_msg = failed_list['status_message']
-        logger.info("Broadcasting failed for the following user: %s, status_code: %s, reason: %s" % (
-            failed_user_id, failed_status_code, failed_status_msg))
-        if failed_status_code == ErrorCodes.RECEIVER_NOT_SUBSCRIBED:
-            logger.info(
-                "User is not subscribed to the Viber Bot - will unsubscribe the user from the new movie newsletter.")
-            db.update_user_subscription_status(failed_user_id, 0)
-            logger.info("%s was unsubscribed from the newsletter successfully.", failed_user_id)
+        for failed_user in failed_list:
+            failed_user_id = failed_user['receiver']
+
+            failed_status_code = failed_user['status']
+            failed_status_msg = failed_user['status_message']
+            logger.warning("Broadcast failed for user: %s, status_code: %s, reason: %s" % (
+                failed_user_id, failed_status_code, failed_status_msg))
+            if failed_status_code == ErrorCodes.RECEIVER_NOT_SUBSCRIBED:
+                logger.info(
+                    "User is not subscribed to the Viber Bot - will unsubscribe the user from the new movies "
+                    "newsletter.")
+                db.update_user_subscription_status(failed_user_id, False)
+                logger.info("'%s' was unsubscribed from the newsletter successfully.", failed_user_id)
+            else:
+                logger.error("Problem with broadcasting to '%s' check status_message: '%s'" % (
+                    failed_user_id, failed_status_msg))
 
 
 broadcast_movies_result = db.fetch_broadcast_movies()
 logger.info("Extracting movies to broadcast")
 for cinema_id, cinema in broadcast_movies_result.items():
+    cinema_name = cinema['cinema_name']
     broadcast_movies = cinema['broadcast_movies']
     if broadcast_movies is not None:
         broadcast_movies = broadcast_movies.split(';')
@@ -67,8 +71,13 @@ for cinema_id, cinema in broadcast_movies_result.items():
         # TODO: Sofia has 2 cinemas - if user is subscribed to either one of them - they should be notified for both
         # TODO: use 'groupId' from API
         if users_to_broadcast:  # Only broadcast to the subscribed users (if any)
-            broadcast_new_movies(broadcast_movies, users_to_broadcast, cinema_id)
-    logger.info("Broadcasting has finished for '%s'. Resetting broadcast_movies for that cinema...",
-                cinema['cinema_name'])
-    # db.reset_broadcast_movies(cinema_id)
-    logger.info("cinemas.broadcast_movies for '%s' has been reset!", cinema['cinema_name'])
+            logger.info("Cinema '%s' - starting a broadcast...", cinema_name)
+            broadcast_new_movies(broadcast_movies, users_to_broadcast, cinema_id, cinema_name)
+        else:
+            logger.info("Cinema '%s' - no users subscribed to that cinema.", cinema_name)
+        logger.info("Cinema '%s' - broadcast has finished. Resetting broadcast_movies for that cinema...",
+                    cinema_name)
+        db.reset_broadcast_movies(cinema_id)
+        logger.info("Cinema '%s' - cinemas.broadcast_movies has been reset!", cinema_name)
+    else:
+        logger.info("Cinema '%s' - nothing to broadcast!", cinema_name)
