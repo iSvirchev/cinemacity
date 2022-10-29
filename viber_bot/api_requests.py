b6c11245ff1c8ed2794ab9976ecb9562520e1946
iSvirchev <i.svirchev@yahoo.com>
2022-10-29 15:01:43 +0300

    api_requests.py:     - fixed a bug where the extracted information will overwrite with each event     - added extra logs

--- viber_bot/api_requests.py
+++ viber_bot/api_requests.py
@@ -23,7 +23,7 @@ cinemas = {}
 def initialize_cinemas():
     url = '%s/quickbook/10106/cinemas/with-event/until/%s?attr=&lang=en_GB' % (API_URL, next_year_date)
     rsp = requests.get(url)
-    log.info("Response is %s for URL '%s'." % (rsp.status_code, API_URL))
+    log.info("Response is %s for URL '%s'." % (rsp.status_code, url))
     if rsp.status_code == 200:
         json_response = json.loads(rsp.text)
         global cinemas
@@ -37,23 +37,33 @@ def pull_cinemas():
     log.info("Will start requesting the available dates for each cinema...")
     for cinema in cinemas:
         cinema_id = cinema['id']
-        # cinema_name = cinema['displayName']
+        cinema_name = cinema['displayName']
         # cinema_image_url = cinema['imageUrl']
+
         url = '%s/quickbook/10106/dates/in-cinema/%s/until/%s?attr=&lang=en_GB' % (API_URL, cinema_id, next_year_date)
         rsp = requests.get(url)
-        log.info("Response is %s for URL '%s'." % (rsp.status_code, API_URL))
+        log.info("Response is %s for URL '%s'." % (rsp.status_code, url))
 
         if rsp.status_code == 200:
             data = json.loads(rsp.text)
             timestamps = data['body']['dates']
+            log.info("Timestamps for %s - '%s' are:" % (cinema_id, cinema_name))
+            log.info(str(timestamps))
+            # The response will sometimes return the dates scrambled
+            # we take the timestamp strings and convert them to datetime objects
             dates = [datetime.datetime.strptime(timestamp, "%Y-%m-%d") for timestamp in timestamps]
             # then we sort the datetime objects via list.sort()
             dates.sort()
             # we convert the datetime objects back to strings
             sorted_dates = [datetime.datetime.strftime(date, "%Y-%m-%d") for date in dates]
+            log.info("The dates have been sorted! Sorted timestamps:")
+            log.info(str(sorted_dates))
 
+            log.info("Will start extracting date info for cinema: %s", cinema_name)
             for date in sorted_dates:
                 pull_date(cinema['id'], date)
+            log.info("All date info extracted for cinema: %s", cinema_name)
+            log.info("=================================================")
         else:
             log.error("The request was not processed properly.")
 
@@ -62,28 +72,32 @@ def pull_date(cinema_id, date):
     url = '%s/quickbook/10106/film-events/in-cinema/%s/at-date/%s?attr=&lang=en_GB' % (
         API_URL, cinema_id, date)
     rsp = requests.get(url)
-    log.info("Response is %s for URL '%s'." % (rsp.status_code, API_URL))
+    log.info("Response is %s for URL '%s'." % (rsp.status_code, url))
 
     if rsp.status_code == 200:
         data = json.loads(rsp.text)
         movies = data['body']['films']
         showings = data['body']['events']
-        movie_screenings = []
+        all_movies = {}
+        all_events = {}
 
+        log.debug("Will start extracting the movies for cinema '%s' for date '%s'", cinema_id, date)
         for movie in movies:
             movie_id = movie['id']
-            poster_link = movie['posterLink'].replace('md', 'sm')  # posters can end in -sm -md -lg depending on size
-            movie_name = movie['name']
-            movie_link = movie['link']
-            trailer_link = movie['videoLink']
+            movie_obj = {'poster_link': movie['posterLink'].replace('md', 'sm'), 'movie_name': movie['name'],
+                         'movie_link': movie['link'], 'trailer_link': movie['videoLink']}
+            all_movies[movie_id] = movie_obj
+            all_events[movie_id] = {'movie_screenings': []}
+        log.debug("Movies extracted!")
+        log.debug("Will start extracting the movie_screenings for cinema '%s' for date '%s'", cinema_id, date)
         for event in showings:
+            film_id = event['filmId']
             event_datetime = datetime.datetime.strptime(event['eventDateTime'], '%Y-%m-%dT%H:%M:%S')
-            movie_screenings.append(event_datetime.strftime('%H:%M'))
+            all_events[film_id]['movie_screenings'].append(event_datetime.strftime('%H:%M'))
+        log.debug("Movie_screenings extracted!")
     else:
         log.error("The request was not processed properly.")
 
 
 initialize_cinemas()
-log.info(cinemas)
-print()
 pull_cinemas()
