beb65a21d3c2e095b20f0d2c51b41b6ce1513606
iSvirchev <i.svirchev@yahoo.com>
2023-06-18 13:25:47 +0300

    - removed unnecessary TOKEN_FILE_PATH from paths.py

--- viber_bot/utility/paths.py
+++ viber_bot/utility/paths.py
@@ -5,5 +5,4 @@ ROOT_DIR = from_root()
 
 LOG_PATH = os.path.join(ROOT_DIR, 'viber_bot', 'logs', 'vbot.log')
 DB_PATH = os.path.join(ROOT_DIR, 'vbot.db')
-TOKEN_FILE_PATH = os.path.join(ROOT_DIR, 'viber_bot', 'misc', 'token_file')
 MOCKED_CINEMAS_PATH = os.path.join(ROOT_DIR, 'viber_bot', 'misc', 'mocked_cinemas.json')
